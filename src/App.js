541b1c1d9f12c8c6bc11abbdd94d4425ad617ca5
alonso1298 <alonsosagrerogranados@gmail.com>
2025-03-24 19:31:24 -0600

    Primer Custom Hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component, useState } from "react";
+import { useState } from "react";
 
 // Reglas de los Hooks: 
 // 1. No se llaman en loops, ni condiciones, siempre en el nivel mas alto del componente
@@ -6,28 +6,36 @@ import { Component, useState } from "react";
 // Componentes de react
 // Custom hooks
 // Cuando creemos un custom hook siempre tiene que comenzar con use*
-class App extends Component {
-    state = {contador: 0}
-    incrementar = () => {
-        this.setState({ contador: this.state.contador + 1})
-    }
-    render () {
-        return (
-            <div>
-                Contador: {this.state.contador}
-                <button onClick={this.incrementar} >Incrementar</button>
-            </div>
-        )
-    }
-}
-// const App = () => {
-//     const [contador, setContador] = useState(0);
-//     return (
-//         <div>
-//             Contador: {contador}
-//             <button onClick={() => setContador(contador + 1)} >Incrementar</button>
-//         </div>
-//     )
+// class App extends Component {
+//     state = {contador: 0}
+//     incrementar = () => {
+//         this.setState({ contador: this.state.contador + 1})
+//     }
+//     render () {
+//         return (
+//             <div>
+//                 Contador: {this.state.contador}
+//                 <button onClick={this.incrementar} >Incrementar</button>
+//             </div>
+//         )
+//     }
 // }
+const useContador = (inicial) => {
+    const [contador, setContador] = useState(inicial);
+    const incrementar = () => {
+        setContador(contador + 1)
+    }
+
+    return [contador, incrementar]
+}
+const App = () => {
+    const [contador, incrementar] = useContador(0);
+    return (
+        <div>
+            Contador: {contador}
+            <button onClick={incrementar} >Incrementar</button>
+        </div>
+    )
+}
 
 export default App;
