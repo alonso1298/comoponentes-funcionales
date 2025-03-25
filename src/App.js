dd69c1ddcbb48e4732da16de0a9f444f82dae833
alonso1298 <alonsosagrerogranados@gmail.com>
2025-03-24 18:31:49 -0600

    useState basado en clases

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { Component, useState } from "react";
 
 // Reglas de los Hooks: 
 // 1. No se llaman en loops, ni condiciones, siempre en el nivel mas alto del componente
 // 2. Solo se llaman es 2 partes:
 // Componentes de react
 // Custom hooks
-const App = () => {
-    const [contador, setContador] = useState(0);
-    return (
-        <div>
-            Contador: {contador}
-            <button onClick={() => setContador(contador + 1)} >Incrementar</button>
-        </div>
-    )
+// Cuando creemos un custom hook siempre tiene que comenzar con use*
+class App extends Component {
+    state = {contador: 0}
+    incrementar = () => {
+        this.setState({ contador: this.state.contador + 1})
+    }
+    render () {
+        return (
+            <div>
+                Contador: {this.state.contador}
+                <button onClick={this.incrementar} >Incrementar</button>
+            </div>
+        )
+    }
 }
+// const App = () => {
+//     const [contador, setContador] = useState(0);
+//     return (
+//         <div>
+//             Contador: {contador}
+//             <button onClick={() => setContador(contador + 1)} >Incrementar</button>
+//         </div>
+//     )
+// }
 
 export default App;
